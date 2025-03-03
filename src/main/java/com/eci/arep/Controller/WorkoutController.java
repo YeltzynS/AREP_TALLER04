d0f1edbbededf4f22a0b44c5b0cc81f2d6c5bb69
YeltzynS <yeltcolo@gmail.com>
2025-03-03 15:09:36 -0500

    lab terminado

--- src/main/java/com/eci/arep/Controller/WorkoutController.java
+++ src/main/java/com/eci/arep/Controller/WorkoutController.java
@@ -1,21 +1,20 @@
 package com.eci.arep.Controller;
 
-import com.eci.arep.Annotation.PostMapping;
-import com.eci.arep.Annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 import com.eci.arep.Server.WorkoutPlanner;
 import java.util.Arrays;
 
-
+@RestController 
 public class WorkoutController {
 
-    @PostMapping("/createWorkout")
-public String createWorkout(@RequestBody String body) {
-    System.out.println("Solicitud recibida: " + body);
-    return "{\"message\": \"Rutina creada con exito\", \"body\": \"" + body + "\"}";
-}
+    @PostMapping("/createWorkout") 
+    public String createWorkout(@RequestBody String body) {
+        System.out.println("Solicitud recibida: " + body);
+        return "{\"message\": \"Rutina creada con exito\", \"body\": \"" + body + "\"}";
+    }
 
-
-    public String getWorkout(String type, String level) {
+    @GetMapping("/getWorkout") 
+    public String getWorkout(@RequestParam String type, @RequestParam String level) {
         String[] workout = WorkoutPlanner.getWorkout(type, level);
         return "{\"workout\": " + Arrays.toString(workout) + "}";
     }
